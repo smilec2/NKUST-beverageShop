35bd5f89ee05266be0b834479d1b0ce8b6233699
Maggiekegebi <innolove71@gmail.com>
2025-02-21 09:45:43 +0800

    20250221 shoppingcarts.balde,新增甜度及版面修正
    更新css、media

--- LotusRoot/resources/views/shoppingcarts.blade.php
+++ LotusRoot/resources/views/shoppingcarts.blade.php
@@ -10,27 +10,31 @@
         <!-- 內容 -->
         <div class="col col-xl-10 mx-auto">
             <form action="" method="post" id="cart-list-form">
-                <table class="table table-hover text-center">
+                <table class="table text-center">
                     <thead class="text-center">
                         <tr>
                             <!-- <tr class="bgc-lotus-a"> -->
                             <th><span class="d-none">選取</span></th>
                             <th scope="col" class="col-7">商品資料</th>
                             <th scope="col" class="col-1">規格</th>
+                            <th scope="col" class="col-2">甜度</th>
                             <th scope="col" class="col-2">數量</th>
                             <th scope="col" class="col-1">小計</th>
                             <th scope="col" class="col-1">變更</th>
                         </tr>
                     </thead>
                     <tbody>
+                        <!-- 1 -->
                         <tr>
                             <!-- 選取 -->
                             <td class="align-middle">
-                                <div class="form-check">
+                                <div
+                                    class="form-check d-flex align-items-center justify-content-center">
                                     <input
+                                        name="product-check"
                                         class="form-check-input"
                                         type="checkbox"
-                                        value=""
+                                        value="1"
                                         id="flexCheckDefault" />
                                     <label class="form-check-label" for="flexCheckDefault">
                                         <span class="d-none">選取</span>
@@ -62,10 +66,22 @@
                                     <option value="3">中</option>
                                 </select>
                             </td>
+                            <!-- 甜度 -->
+                            <td class="align-middle">
+                                <select
+                                    class="form-select text-center border-1 bg-transition text-darkred rounded"
+                                    aria-label="select-list">
+                                    <!-- <option selected>請選擇</option> -->
+                                    <option value="1" selected>固定</option>
+                                    <option value="2">微</option>
+                                    <option value="3">半</option>
+                                    <option value="4">正常</option>
+                                </select>
+                            </td>
                             <!-- 數量 -->
                             <td class="align-middle">
-                                <div class="d-flex allign-items-center">
-                                    <button class="btn dash-btn" type="button">
+                                <div class="d-flex flex-lg-row align-items-center">
+                                    <button class="btn p-1 dash-btn" type="button">
                                         <i class="bi bi-dash-lg"></i>
                                     </button>
                                     <select
@@ -75,14 +91,14 @@
                                         <option value="2">2</option>
                                         <option value="3">3</option>
                                     </select>
-                                    <button class="btn plus-btn" type="button">
+                                    <button class="btn p-1 plus-btn" type="button">
                                         <i class="bi bi-plus-lg"></i>
                                     </button>
                                 </div>
                             </td>
                             <!-- 小計 -->
-                            <td class="align-middle">
-                                <span value="55">55</span>
+                            <td class="align-middle text-end p-3">
+                                <span class="p-3" value="55">55</span>
                             </td>
                             <!-- 變更 -->
                             <td class="align-middle">
@@ -100,14 +116,17 @@
                                 </div>
                             </td>
                         </tr>
+                        <!-- 2 -->
                         <tr>
                             <!-- 選取 -->
                             <td class="align-middle">
-                                <div class="form-check">
+                                <div
+                                    class="form-check d-flex align-items-center justify-content-center">
                                     <input
+                                        name="product-check"
                                         class="form-check-input"
                                         type="checkbox"
-                                        value=""
+                                        value="1"
                                         id="flexCheckDefault" />
                                     <label class="form-check-label" for="flexCheckDefault">
                                         <span class="d-none">選取</span>
@@ -139,10 +158,22 @@
                                     <option value="3">中</option>
                                 </select>
                             </td>
+                            <!-- 甜度 -->
+                            <td class="align-middle">
+                                <select
+                                    class="form-select text-center border-1 bg-transition text-darkred rounded"
+                                    aria-label="select-list">
+                                    <!-- <option selected>請選擇</option> -->
+                                    <option value="1" selected>固定</option>
+                                    <option value="2">微</option>
+                                    <option value="3">半</option>
+                                    <option value="4">正常</option>
+                                </select>
+                            </td>
                             <!-- 數量 -->
                             <td class="align-middle">
-                                <div class="d-flex allign-items-center">
-                                    <button class="btn dash-btn" type="button">
+                                <div class="d-flex flex-lg-row align-items-center">
+                                    <button class="btn p-1 dash-btn" type="button">
                                         <i class="bi bi-dash-lg"></i>
                                     </button>
                                     <select
@@ -152,14 +183,14 @@
                                         <option value="2">2</option>
                                         <option value="3">3</option>
                                     </select>
-                                    <button class="btn plus-btn" type="button">
+                                    <button class="btn p-1 plus-btn" type="button">
                                         <i class="bi bi-plus-lg"></i>
                                     </button>
                                 </div>
                             </td>
                             <!-- 小計 -->
-                            <td class="align-middle">
-                                <span value="55">55</span>
+                            <td class="align-middle text-end p-3">
+                                <span class="p-3" value="55">55</span>
                             </td>
                             <!-- 變更 -->
                             <td class="align-middle">
@@ -177,14 +208,17 @@
                                 </div>
                             </td>
                         </tr>
+                        <!-- 3 -->
                         <tr>
                             <!-- 選取 -->
                             <td class="align-middle">
-                                <div class="form-check">
+                                <div
+                                    class="form-check d-flex align-items-center justify-content-center">
                                     <input
+                                        name="product-check"
                                         class="form-check-input"
                                         type="checkbox"
-                                        value=""
+                                        value="1"
                                         id="flexCheckDefault" />
                                     <label class="form-check-label" for="flexCheckDefault">
                                         <span class="d-none">選取</span>
@@ -216,10 +250,22 @@
                                     <option value="3">中</option>
                                 </select>
                             </td>
+                            <!-- 甜度 -->
+                            <td class="align-middle">
+                                <select
+                                    class="form-select text-center border-1 bg-transition text-darkred rounded"
+                                    aria-label="select-list">
+                                    <!-- <option selected>請選擇</option> -->
+                                    <option value="1" selected>固定</option>
+                                    <option value="2">微</option>
+                                    <option value="3">半</option>
+                                    <option value="4">正常</option>
+                                </select>
+                            </td>
                             <!-- 數量 -->
                             <td class="align-middle">
-                                <div class="d-flex allign-items-center">
-                                    <button class="btn dash-btn" type="button">
+                                <div class="d-flex flex-lg-row align-items-center">
+                                    <button class="btn p-1 dash-btn" type="button">
                                         <i class="bi bi-dash-lg"></i>
                                     </button>
                                     <select
@@ -229,14 +275,14 @@
                                         <option value="2">2</option>
                                         <option value="3">3</option>
                                     </select>
-                                    <button class="btn plus-btn" type="button">
+                                    <button class="btn p-1 plus-btn" type="button">
                                         <i class="bi bi-plus-lg"></i>
                                     </button>
                                 </div>
                             </td>
                             <!-- 小計 -->
-                            <td class="align-middle">
-                                <span value="55">55</span>
+                            <td class="align-middle text-end p-3">
+                                <span class="p-3" value="55">55</span>
                             </td>
                             <!-- 變更 -->
                             <td class="align-middle">
@@ -257,9 +303,24 @@
                     </tbody>
                 </table>
                 <!-- 結帳 -->
-                <div class="text-end bgc-lotus-a p-3">
-                    <span class="text-darkred me-3">結帳金額</span>
-                    <span class="text-darkred fs-4 card-money-text">165</span>
+                <div
+                    class="d-flex align-items-center justify-content-between bgc-lotus-a p-md-3 p-3">
+                    <div class="col-4">
+                        <input
+                            name="product-check-all"
+                            class="form-check-input me-2"
+                            type="checkbox"
+                            value=""
+                            id="product-check-all" />
+                        <label class="text-darkred" for="product-check-all">全選 </label>
+                    </div>
+                    <div>
+                        <span class="text-darkred">已選 <span id="selected-count">0</span> 項商品
+                        </span>
+
+                        <span class="text-darkred me-3">，結帳金額</span>
+                        <span class="text-darkred fs-4 card-money-text">165</span>
+                    </div>
                 </div>
                 <!-- 結帳btn -->
                 <div class="text-end mt-4">
