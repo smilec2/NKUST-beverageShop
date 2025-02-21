ab17d88226a4186e4ebaf921131074c243e1e930
jay <jay879944@gmail.com>
2025-02-21 09:37:25 +0800

    fixed bug of oppening logic modal

--- LotusRoot/app/Http/Controllers/SocialiteController.php
+++ LotusRoot/app/Http/Controllers/SocialiteController.php
@@ -5,23 +5,26 @@ use App\Http\Controllers\Controller;
 use Socialite;
 class SocialiteController extends Controller
 {
-/**
-* Redirect the user to the Google authentication page.
-*
-* @return 301
-*/
-public function redirectToProvider()
-{
-return Socialite::driver('google')->redirect();
-}
-/**
-* Obtain the user information from Google.
-*
-* @return \Illuminate\Http\Response
-*/
-public function handleProviderCallback()
-{
-$user = Socialite::driver('google')->stateless()->user();
-dd($user);
-}
+    /**
+    * Redirect the user to the Google authentication page.
+    *
+    * @return 301
+    */
+
+    public function redirectToProvider()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    /**
+    * Obtain the user information from Google.
+    *
+    * @return \Illuminate\Http\Response
+    */
+
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('google')->stateless()->user();
+        dd($user);
+    }
 }
